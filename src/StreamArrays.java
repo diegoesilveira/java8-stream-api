e76855c5711266fec19890ad10dca29c4dd26cd0
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2019-05-19 21:38:08 -0300

    Criado a classe StreamArrays, exemplificar como usar Stream com arrays

--- src/StreamArrays.java
+++ src/StreamArrays.java
@@ -5,16 +5,32 @@ public class StreamArrays {
 	public static void main(String[] args) {
 
 		Arrays.stream(new int[] { 0, 1, 2, 3, 4, 5, 10, 11, 50, 51 })
-		.max()
-		.ifPresent(System.out::println);
+			.max()
+			.ifPresent(System.out::println);
 
-		Arrays.stream(new int[] { 0, 1, 2, 3, 4, 5, 10, 11, 50, 51 })
-		.min()
-		.ifPresent(System.out::println);
+		Arrays.stream(new int[] { -1, -10, 0, 1, 2, 3, 4, 5, 10, 11, 50, 51 })
+			.min()
+			.ifPresent(System.out::println);
 
-		Arrays.stream(new int[] { 1, 2, 3, 4, 5, 10, 11, 50, 51 })
-		.average()
-		.ifPresent(System.out::println);
+		double asDouble = Arrays.stream(new int[] {0, 1, 1, 3, 2, 3, 4, 5, 10, 11, 50, 51 })
+			.distinct()
+			.average()
+			.getAsDouble();
+			
+		System.out.println("Média array: " +(int)asDouble);
+		
+		long countArray = Arrays.stream(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+			.count();
+		
+		System.out.println("Conta quantas posições tem o array: " +countArray);
+		
+		long sumArrays = Arrays.stream(new int[] { 1, 1, 3, 3, 4, 4, 5, 5, 2, 3, 4, 5, 10, 11, 50, 51 })
+			.distinct()
+			.sum();
+			
+		System.out.println("Soma arrays diferentes: " +sumArrays);
+		
+		
 
 	}
 
